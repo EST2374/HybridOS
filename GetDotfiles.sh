f0df018c7e2c78b075f9410577e1738d9116840c
EST2374 <jack.cez2004@gmail.com>
2026-01-03 19:28:37 +0100

    little script fix

--- GetDotfiles.sh
+++ GetDotfiles.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 Confs=('alacritty' 'autostart' 'btop' 'dconf' 'elephant' 'freerdp'
-  'go' 'hypr' 'KDE' 'kdedefaults' 'nautilus' 'plasma-workspace' 'pulse'
+  'go' 'hypr' 'KDE' 'kdedefaults' 'plasma-workspace' 'pulse'
   'qalculate' 'systemd' 'vlc' 'waybar' 'walker')
 
 config_pth=~/.config/
 
 printf "\nStart copying config Files\n\n"
 for conf in "${Confs[@]}"; do
-  cp -r $config_pth$conf/* $conf/
+  cp -r $config_pth$conf/* ./config/$conf
   printf "%s copied\n" $conf
 done
 
