fffed499bb5a2c7f9255f89ed23b08b72cea268d
EST2374 <jack.cez2004@gmail.com>
2026-01-10 21:47:47 +0100

    Firewall and new GameLauncher add

--- installer.sh
+++ installer.sh
@@ -60,7 +60,8 @@ sudo pacman -S --needed --noconfirm \
   openbsd-netcat \
   wiremix \
   swayosd \
-  pamixer
+  pamixer \
+  ufw
 
 # If yay isnt installed
 if ! command -v yay &>/dev/null; then
@@ -83,6 +84,29 @@ yay -S --noconfirm \
 
 # Enable systemctl Stuff
 systemctl enable --now NetworkManager.service
+sudo systemctl enable --now swayosd-libinput-backend.service
+
+# Firewall setup
+# Allow nothing in, everything out
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+
+# Allow ports for LocalSend
+sudo ufw allow 53317/udp
+sudo ufw allow 53317/tcp
+
+# Allow Docker containers to use DNS on host
+sudo ufw allow in proto udp from 172.16.0.0/12 to 172.17.0.1 port 53 comment 'allow-docker-dns'
+
+# Turn on the firewall
+sudo ufw --force enable
+
+# Enable UFW systemd service to start on boot
+sudo systemctl enable ufw
+
+# Turn on Docker protections
+sudo ufw-docker install
+sudo ufw reload
 
 # Repo stuff
 SHARE_PATH="$HOME/.local/share/HybridOS"
